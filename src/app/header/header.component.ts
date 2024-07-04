b9ead24eb24a21f9262bad51e5c5cf01c70be564
omar karake <omar.karake@amalitechtraining.org>
2024-07-04 20:44:28 +0200

    dark mode detect codes then apply automatically

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   isDarkMode = false;
 
-  toggleDarkMode() {
-    
+  ngOnInit(): void {
+    // Check initial system preference
+    this.checkSystemDarkMode();
+
+    // Add event listener for system dark mode changes
+    window
+      .matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', this.checkSystemDarkMode.bind(this));
+  }
+
+  toggleDarkMode(): void {
     this.isDarkMode = !this.isDarkMode;
-    if (this.isDarkMode) {
+    this.applyDarkMode(this.isDarkMode);
+  }
+
+  private checkSystemDarkMode(event?: MediaQueryListEvent): void {
+    const prefersDarkMode = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    ).matches;
+    this.isDarkMode = event ? event.matches : prefersDarkMode;
+    this.applyDarkMode(this.isDarkMode);
+  }
+
+  private applyDarkMode(isDarkMode: boolean): void {
+    if (isDarkMode) {
       document.documentElement.classList.add('dark');
     } else {
       document.documentElement.classList.remove('dark');
