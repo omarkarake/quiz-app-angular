03823fdf8688c128cf9c5686928cc29c052f341a
omar karake <omar.karake@amalitechtraining.org>
2024-07-04 11:43:41 +0200

    setting the tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: "#A729F5",
+        "dark-navy": "#313E51",
+        navy: "#3B4D66",
+        "grey-navy": "#626C7F",
+        "light-blueish": "#ABC1E1",
+        "light-grey": "#F4F6FA",
+        "pure-white": "#FFFFFF",
+        green: "#26D782",
+        red: "#EE5454",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
