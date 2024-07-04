fe5bd8067d898b4763254bbdd05cda5647b14586
omar karake <omar.karake@amalitechtraining.org>
2024-07-04 15:24:53 +0200

    done implementing the header section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'], // Corrected to styleUrls
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'quiz-app';
@@ -14,11 +14,31 @@ export class AppComponent {
     private matIconRegistry: MatIconRegistry,
     private domSanitizer: DomSanitizer
   ) {
-    this.matIconRegistry.addSvgIcon(
-      'accessibility',
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        'assets/images/icon-accessibility.svg' // Updated path
-      )
-    );
+    const icons = [
+      'icon-accessibility',
+      'icon-correct',
+      'icon-css',
+      'icon-error',
+      'icon-html',
+      'icon-incorrect',
+      'icon-js',
+      'icon-moon-dark',
+      'icon-moon-light',
+      'icon-sun-dark',
+      'icon-sun-light',
+      'pattern-background-desktop-dark',
+      'pattern-background-desktop-light',
+      'pattern-background-mobile-dark',
+      'pattern-background-mobile-light',
+      'pattern-background-tablet-dark',
+      'pattern-background-tablet-light'
+    ];
+
+    icons.forEach(icon => {
+      this.matIconRegistry.addSvgIcon(
+        icon,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/images/${icon}.svg`)
+      );
+    });
   }
 }
