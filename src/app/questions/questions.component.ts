080fea10ce1341c7daadf650c724ad3760750803
omar karake <omar.karake@amalitechtraining.org>
2024-07-05 15:18:34 +0200

    questions incorrect and question correct before submit

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -24,6 +24,8 @@ export class QuestionsComponent implements OnInit {
   hoveredOption: number | null = null;
   selectedOption: number | null = null;
   showError: boolean = false;
+  showFeedback: boolean = false;
+  isAnswerCorrect: boolean = false;
 
   ngOnInit() {
     if (this.quiz?.questions?.length) {
@@ -52,23 +54,23 @@ export class QuestionsComponent implements OnInit {
     if (this.selectedOption === null) {
       this.showError = true;
     } else {
-      // Handle answer submission logic here, e.g., check if the answer is correct
-      if (this.currentQuestionIndex < (this.quiz?.questions.length ?? 0) - 1) {
-        this.nextQuestion();
-      } else {
-        console.log('End of quiz, handle final submission');
-        // Handle end of quiz logic here
+      const currentQuestion = this.getCurrentQuestion();
+      if (currentQuestion) {
+        this.isAnswerCorrect = currentQuestion.options[this.selectedOption] === currentQuestion.answer;
+        this.showFeedback = true;
       }
     }
   }
 
   nextQuestion(): void {
-    if (this.selectedOption === null) {
-      this.showError = true;
-    } else {
+    if (this.currentQuestionIndex < (this.quiz?.questions?.length ?? 0) - 1) {
       this.currentQuestionIndex++;
       this.selectedOption = null;
       this.showError = false;
+      this.showFeedback = false;
+    } else {
+      console.log('End of quiz, handle final submission');
+      // Handle end of quiz logic here
     }
   }
 }
