2bb0f8431c375b9440ab79785e8c74b05a3ef134
omar karake <omar.karake@amalitechtraining.org>
2024-07-20 21:24:02 +0200

    test(dark-mode-service): add test for setDarkMode method
    - Added a test case to verify that the setDarkMode method correctly updates the darkMode BehaviorSubject.
    - Ensured the observable darkMode$ emits the expected value when setDarkMode is called.

--- src/app/dark-mode.service.spec.ts
+++ src/app/dark-mode.service.spec.ts
@@ -1,16 +1,27 @@
-import { TestBed } from '@angular/core/testing';
-
+import { take } from 'rxjs';
 import { DarkModeService } from './dark-mode.service';
 
 describe('DarkModeService', () => {
   let service: DarkModeService;
+  let httpClientSpy: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(DarkModeService);
+    httpClientSpy = {
+      get: jest.fn(),
+    };
+    service = new DarkModeService();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should setDarkMode set darkMode', (done) => {
+    const isDarkMode = true;
+    service.setDarkMode(isDarkMode);
+    service.darkMode$.pipe(take(1)).subscribe((value) => {
+      expect(value).toBe(isDarkMode);
+      done();
+    });
   });
 });
